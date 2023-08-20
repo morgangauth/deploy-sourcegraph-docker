d037b0e938aec61bbabc216902ead53ffced48df
DaedalusG <warrenbruceg@gmail.com>
2023-08-20 01:47:50 -0700

    testing streaming output

--- test/upgrade-test.go
+++ test/upgrade-test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
@@ -19,9 +20,15 @@ func main() {
 
     if testType == "standard" {
         testStandardUpgrade(versions)
+		if err := performStandardUpgrade(versions); err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
     } else if (testType == "multiversion" || testType == "mvu") {
         testMultiversionUpgrade(versions)
-    }
+    } else {
+		fmt.Println("Error: Must declare testType as 'standard' or 'multiversion'")
+	}
 }
 
 func testStandardUpgrade(versions []string) error {
@@ -49,7 +56,7 @@ func testMultiversionUpgrade(versions []string) error {
 }
 
 func checkVersion(versions []string) error {
-	versionRegex, nil := regexp.Compile(`^v?(\d+\.\d+\.\d+)$`)
+	versionRegex, nil := regexp.Compile(`^v?(\d+\.\d+\.\d+)`)
 	for _, version := range versions {
 		if !versionRegex.MatchString(version) {
 			return fmt.Errorf("invalid version %s", version)
@@ -65,20 +72,32 @@ func checkRepo() error {
 	  }
 	
 	crs := strings.TrimSpace(string(cr))
-	fmt.Println(crs)
   
 	if !strings.Contains(crs, "deploy-sourcegraph-docker") {
 	  return fmt.Errorf("Must run from deploy-sourcegraph-docker repository") 
 	}
   
 	return nil
-  }
+}
 
-  func performStandardUpgrade(versions []string) error {
-
+func performStandardUpgrade(versions []string) error {
   // Prune docker volumes
-  if err := exec.Command("docker", "volume", "prune", "-f").Run(); err != nil {
-    return fmt.Errorf("error pruning docker volumes: %s", err)
+  cmd := exec.Command("docker", "volume", "prune", "-f")
+  stdout, _ := cmd.StdoutPipe()
+  stderr, _ := cmd.StderrPipe()
+  if err := cmd.Start(); err != nil {
+        return fmt.Errorf("error starting docker volume prune: %v", err)
+  }
+  stdScanner := bufio.NewScanner(stdout)
+  for stdScanner.Scan() {
+    fmt.Println(stdScanner.Text()) 
+  }
+  errScanner := bufio.NewScanner(stderr)
+  for errScanner.Scan() {
+    fmt.Println(errScanner.Text()) 
+  }
+  if err := cmd.Wait(); err != nil {
+	return err
   }
 
   for i, version := range versions {
@@ -87,29 +106,41 @@ func checkRepo() error {
     if !strings.HasPrefix(version, "v") {
 		versions[i] = "v" + version
 	}
-    if err := exec.Command("git", "checkout", version).Run(); err != nil {
+    cmd := exec.Command("git", "checkout", version)
+	stdout, _ := cmd.StdoutPipe()
+    stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
       return fmt.Errorf("error checking out %s: %s", version, err)
     }
-
-    // Docker compose up
-    cmd := exec.Command("docker-compose", "up", "-d")
-    cmd.Dir = "deploy-sourcegraph-docker/docker-compose" 
-    if err := cmd.Run(); err != nil {
-      return fmt.Errorf("error running docker-compose up for %s: %s", version, err)
+	stdScanner := bufio.NewScanner(stdout)
+    for stdScanner.Scan() {
+      fmt.Println(stdScanner.Text()) 
+    }
+    errScanner := bufio.NewScanner(stderr)
+    for errScanner.Scan() {
+      fmt.Println(errScanner.Text()) 
+    }
+    if err := cmd.Wait(); err != nil {
+	  return err
     }
 
-    if i < len(versions)-1 {
-      // Docker compose down
-      cmd := exec.Command("docker-compose", "down", "--remove-orphans")
-      cmd.Dir = "deploy-sourcegraph-docker/docker-compose"
-      if err := cmd.Run(); err != nil {
-        return fmt.Errorf("error running docker-compose down for %s: %s", version, err)  
-      }
-    }
+    // // Docker compose up
+    // cmd := exec.Command("docker-compose", "up", "-d")
+	// cmd.Dir = "deploy-sourcegraph-docker/docker-compose"
+    // out, err = cmd.Output()
+	// if err != nil {
+  	//   return fmt.Errorf("error running docker-compose up for %s: %s", version, err)
+	// }
+	// fmt.Println(string(out))
+
+    // // Docker compose down
+    // cmd = exec.Command("docker-compose", "down", "--remove-orphans")
+    // out, err = cmd.Output()
+	// if err != nil {
+    //   return fmt.Errorf("error running docker-compose down for %s: %s", version, err)  
+    // }
+	// fmt.Println(string(out))
 
   }
-
   return nil
-
 }
-  
